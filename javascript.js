34b7e77980f29e0ce486d7f7b31c90e181f9e577
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-08-22 14:10:32 +0200

    Replaced Google Maps by Bing Maps.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(53.21917, 6.56667),
-        zoom: 13,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+let map;
+
+function GetMap() {
+    map = new Microsoft.Maps.Map('#bingMap', {
+        center: new Microsoft.Maps.Location(53.21917, 6.56667),
+        mapTypeId: Microsoft.Maps.MapTypeId.load,
+        zoom: 13
+    });
 }
